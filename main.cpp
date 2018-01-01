d3564a1e091d2adfe9fddb16de3a7450faf000ae
souravdas25 <souravbumbadas25@gmail.com>
2018-01-02 00:27:29 +0530

    Parser Added

--- main.cpp
+++ main.cpp
@@ -2,30 +2,31 @@
 
 
 #include <iostream>
-#include "include/JsonStore.h"
+#include "include/Parser.h"
 
 int main() {
-    using StaticDB::sdb_error;
+    using namespace StaticDB;
 
-    StaticDB::JsonStore js;
     try {
-        js.open("testdb", "root");
-        //dm.printBase(std::cout);
 
-        //dm.erase(2);
-//        js.store(1,"Sourav Das is a good boy.");
-//        js.store(2,23);
-//        js.store(3,5.36);
-        json j2 = js.getJson(4);
+        json j2 = {
+                {"pi",      3.141},
+                {"happy", true},
+                {"name",    "Niels"},
+                {"nothing", nullptr},
+                {"answer",  {
+                                    {"everything", 42}
+                            }},
+                {"list",    {       1, 0, 2}},
+                {"object",  {
+                                    {"currency",   "USD"},
+                                       {"value", 42.99}
+                            }}
+        };
+
+        std::cout << Parser::walkJson("list[2]",j2);
 
 
-        //js.storeJson(4,j2);
-        std::cout << j2;
-
-        js.printBase(std::cout);
-
-        js.commit();
-        js.close();
     }
     catch (const std::exception &e) {
         std::cout << e.what();
